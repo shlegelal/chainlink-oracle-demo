aaf5c7961ca90a22d3110ead845ee431a85fea01
shlegelal <shlegel.alink0@gmail.com>
2023-12-08 18:55:47 +0300

    init ganache

---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    contracts_directory: "./contracts",
+    contracts_build_directory: "./build",
+    networks: {
+        ganache: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*"
+        }
+    },
+    compilers: {
+        solc: {
+            version: "^0.8.0"
+        }
+    }
+}
