ac8800b70aa4c4cb02bbf690c9814c22edc061c4
shlegelal <shlegel.alink0@gmail.com>
2024-04-16 18:22:46 +0300

    update migrations

--- truffle/migrations/2-deploy-client-and-fund.js
+++ truffle/migrations/2-deploy-client-and-fund.js
@@ -13,4 +13,11 @@ module.exports = async (deployer) => {
     addrFile = path.join(__dirname, '..', '..', 'config', 'addrs.env');
 
     fs.writeFileSync(addrFile,`CLIENT_CONTRACT_ADDRESS=${Client.address}\n`, {flag: "a+"});
+
+    const tokenAddress = await Client.getChainlinkToken();
+    const token = await LinkToken.at(tokenAddress);
+
+    console.log(`Transferring 50 LINK to GanacheClient (address ${Client.address})...`);
+    let receipt = await token.transfer(Client.address, `50`);
+    console.log(`LINK Transfer succeeded! Transaction ID: ${receipt.tx}.`);
 };
